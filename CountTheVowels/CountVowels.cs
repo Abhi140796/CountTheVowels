d3d3c3b0effe3c0515086c64ebad5fe8a509859d
Abhishek Dubey <abhishekd905@gmail.com>
2021-12-26 16:10:26 +0530

    Add project files.

---
+++ CountTheVowels/CountVowels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheVowels
+{
+    class CountVowels
+    {
+        public static int CountVowelsInWord(string enterEnglishLetter)
+        {
+            string EnglishLetterLowerCase = enterEnglishLetter.ToLower();
+            int lengthOfString = enterEnglishLetter.Length;
+            int countVowels = 0;
+            for(int i = 0; i < lengthOfString; i++)
+            {
+                if(EnglishLetterLowerCase[i] == 'a' || EnglishLetterLowerCase[i] == 'e' || EnglishLetterLowerCase[i] == 'i' || EnglishLetterLowerCase[i] == 'o' || EnglishLetterLowerCase[i] == 'u')
+                {
+                    countVowels++;
+                }
+            }
+            return countVowels;
+        }
+    }
+}
---
+++ CountTheVowels/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CountTheVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Word: ");
+            string englishWord = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Number of vowels: {0}",CountVowels.CountVowelsInWord(englishWord));
+        }
+    }
+}
